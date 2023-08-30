4a374c13b1977002570e67dce1681a32b0a301af
ayman-anwar <122607921+ayman-anwar@users.noreply.github.com>
2023-08-30 19:27:03 +0530

    .

--- OptimalDrilling(TSP)(7).py
+++ OptimalDrilling(TSP)(7).py
@@ -1,1 +1,36 @@
+import networkx as nx
+
+def get_distances(num_nodes):
+  distances={}
+  for i in range(1,num_nodes+1):
+    for j in range(i+1,num_nodes+1):
+      distance=float(input(f"Enter the distance between node {i} and node {j} : "))
+      distances[(i,j)]=distance
+      distances[(j,i)]=distance
+  return distances
+
+def tap_optimal_drilling(distances):
+  G=nx.Graph()
+  G.add_weighted_edges_from((i,j,distance) for (i,j),distance in distances.items())
+  optimal_order=nx.approximation.traveling_salesman_problem(G,cycle=True)
+  return optimal_order
+
+def calculate_optimal_cost(drill_order,distances):
+  total_cost = sum(distances[(drill_order[i]),(drill_order[i+1])] for i in range(len(drill_order)-1))
+  return total_cost
+
+if __name__=='__main__':
+  while True:
+    n=int(input("Enter no. of nodes: "))
+    distances=get_distances(n)
+
+    optimal_order=tap_optimal_drilling(distances)
+    optimal_cost=calculate_optimal_cost(optimal_order,distances)
+
+    print("Optimal order: ",optimal_order)
+    print("Optimal cost: ",optimal_cost)
+
+    c=input("Press yes to continue: ").lower()
+    if c!='yes':
+      break
 
